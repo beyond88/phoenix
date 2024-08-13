395d47237307899b98a5ca0d2bda197242fa92e8
rejuancse <rejuancse@gmail.com>
2024-08-13 19:28:42 +0600

    Admin dashboard menu updated

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- ===============================================-->
-    <!--    JavaScripts-->
+    <!--    JavaScripts -->
     <!-- ===============================================-->
     <script src="{{ url('js/popper.min.js') }}"></script>
     <script src="{{ url('js/bootstrap.min.js') }}"></script>
@@ -7,7 +7,7 @@
     <script src="{{ url('js/is.min.js') }}"></script>
     <script src="{{ url('js/all.min.js') }}"></script>
     <script src="{{ url('js/lodash.min.js') }}"></script>
-    <script src="https://polyfill.io/v3/polyfill.min.js'?features=window.scroll"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@babel/polyfill/dist/polyfill.min.js"></script>
     <script src="{{ url('js/list.min.js') }}"></script>
     <script src="{{ url('js/feather.min.js') }}"></script>
     <script src="{{ url('js/dayjs.min.js') }}"></script>
@@ -21,7 +21,6 @@
     <script src="{{ url('js/leaflet-tilelayer-colorfilter.min.js') }}"></script>
     <script src="{{ url('js/echarts.min.js') }}"></script>
     <script src="{{ url('js/ecommerce-dashboard.js') }}"></script>
-
   </body>
 
 </html>
