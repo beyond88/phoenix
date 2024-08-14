a3a7baaf7d24d7c4602f2f7d3e336ea93f5db7e9
beyond88 <muhin.cse.diu@gmail.com>
2024-08-14 16:13:54 +0600

    dynamic sidebar url

---
+++ resources/views/backend/reviews/add-new.blade.php
@@ -0,0 +1,20 @@
+@extends("backend.layouts.layout")
+
+@section("title", "Add New Review | Phoenix")
+
+@section("content")    
+    
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+
+        @include("backend.layouts.sidebar")
+
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+@endsection
