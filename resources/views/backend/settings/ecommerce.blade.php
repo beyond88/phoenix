12fe7c23c563b576f2636205f97faefb4823ee6d
beyond88 <muhin.cse.diu@gmail.com>
2024-08-20 00:57:17 +0600

    add ecommerce blade

--- resources/views/backend/settings/ecommerce.blade.php
+++ resources/views/backend/settings/ecommerce.blade.php
@@ -1,20 +1,76 @@
 @extends("backend.layouts.layout")
-
-@section("title", "Ecommerce Settings | Phoenix")
-
+@section("title", "Posts | Phoenix")
 @section("content")    
-    
-    <!-- ===============================================-->
-    <!--    Main Content-->
-    <!-- ===============================================-->
-    <main class="main" id="top">
-
-        @include("backend.layouts.sidebar")
-
-
-    </main>
-    <!-- ===============================================-->
-    <!--    End of Main Content-->
-    <!-- ===============================================-->
-
+<!-- ===============================================-->
+<!--    Main Content-->
+<!-- ===============================================-->
+<main class="main" id="top">
+   @include("backend.layouts.sidebar")
+   <div class="content">
+      <nav class="mb-2" aria-label="breadcrumb">
+         <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item"><a href="#!">Page 1</a></li>
+            <li class="breadcrumb-item"><a href="#!">Page 2</a></li>
+            <li class="breadcrumb-item active">Default</li>
+         </ol>
+      </nav>
+      <div class="mb-9">
+         <div class="row g-3 mb-4">
+            <div class="col-auto">
+               <h2 class="mb-0">Settings</h2>
+            </div>
+         </div>
+        <div class="mt-4">
+          <div class="row g-4">
+            <div class="col-12 col-xl-10 order-1 order-xl-0">
+              <div class="mb-9">
+                <div class="card shadow-none border my-4" data-component-card="data-component-card">
+                  <div class="card-header p-4 border-bottom bg-body">
+                    <div class="row g-3 justify-content-between align-items-end">
+                      <div class="col-12 col-md">
+                        <h4 class="text-body mb-0" data-anchor="data-anchor">Basic Example</h4>
+                        <p class="mb-0 mt-2 text-body-secondary">Takes the basic nav and adds the <code>.nav-tabs</code> class to generate a tabbed interface. Use them to create tabbable regions with our tab JavaScript plugin.</p>
+                      </div>
+                      <div class="col col-md-auto">
+                        <nav class="nav justify-content-end doc-tab-nav align-items-center" role="tablist">
+                          <button class="btn btn-link px-2 text-body copy-code-btn" type="button"><span class="fas fa-copy me-1"></span>Copy Code</button><a class="btn btn-sm btn-phoenix-primary code-btn ms-2" data-bs-toggle="collapse" href="#basic-example-code" role="button" aria-controls="basic-example-code" aria-expanded="false"> <span class="me-2" data-feather="code"></span>View code</a><a class="btn btn-sm btn-phoenix-primary preview-btn ms-2"><span class="me-2" data-feather="eye"></span>Preview</a>
+                        </nav>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body p-0">
+                    <div class="p-4 code-to-copy">
+                      <ul class="nav nav-underline fs-9" id="myTab" role="tablist">
+                        <li class="nav-item"><a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#tab-home" role="tab" aria-controls="tab-home" aria-selected="true">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#tab-profile" role="tab" aria-controls="tab-profile" aria-selected="false">Profile</a></li>
+                        <li class="nav-item"><a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#tab-contact" role="tab" aria-controls="tab-contact" aria-selected="false">Contact</a></li>
+                      </ul>
+                      <div class="tab-content mt-3" id="myTabContent">
+                        <div class="tab-pane fade show active" id="tab-home" role="tabpanel" aria-labelledby="home-tab">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone.</div>
+                        <div class="tab-pane fade" id="tab-profile" role="tabpanel" aria-labelledby="profile-tab">Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic.</div>
+                        <div class="tab-pane fade" id="tab-contact" role="tabpanel" aria-labelledby="contact-tab">Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="footer position-absolute">
+         <div class="row g-0 justify-content-between align-items-center h-100">
+            <div class="col-12 col-sm-auto text-center">
+               <p class="mb-0 mt-2 mt-sm-0 text-body">Thank you for creating with Phoenix<span class="d-none d-sm-inline-block"></span><span class="d-none d-sm-inline-block mx-1">|</span><br class="d-sm-none" />2024 &copy;<a class="mx-1" href="https://themewagon.com">Themewagon</a></p>
+            </div>
+            <div class="col-12 col-sm-auto text-center">
+               <p class="mb-0 text-body-tertiary text-opacity-85">v1.17.0</p>
+            </div>
+         </div>
+      </footer>
+   </div>
+</main>
+<!-- ===============================================-->
+<!--    End of Main Content-->
+<!-- ===============================================-->
 @endsection
