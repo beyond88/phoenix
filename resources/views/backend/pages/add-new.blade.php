b88d6693650395c288508b016dc73023d77fa09c
beyond88 <muhin.cse.diu@gmail.com>
2024-08-21 23:50:57 +0600

    get update from main branch

--- resources/views/backend/pages/add-new.blade.php
+++ resources/views/backend/pages/add-new.blade.php
@@ -86,7 +86,7 @@
                 <div class="col-12 col-sm-auto text-center">
                     <p class="mb-0 mt-2 mt-sm-0 text-body">Thank you for creating with Phoenix <span class="d-none d-sm-inline-block"></span>
                         <span class="d-none d-sm-inline-block mx-1">|</span>
-                        <br class="d-sm-none" />2024 &copy; <a class="mx-1" href="https://themewagon.com">Themewagon</a>
+                        <br class="d-sm-none" />2024 &copy; <a class="mx-1" href="#">Themewagon</a>
                     </p>
                 </div>
                 <div class="col-12 col-sm-auto text-center">
