dd268b3ba8fbedb25799373374971d91abc9c2ad
beyond88 <muhin.cse.diu@gmail.com>
2024-08-22 01:18:08 +0600

    add copyright blade

--- app/Livewire/Posts/Categories.php
+++ app/Livewire/Posts/Categories.php
@@ -3,16 +3,17 @@
 namespace App\Livewire\Posts;
 
 use Livewire\Component;
-// use App\Models\Categories as Posts;
+use App\Models\PostCategory;
 
 
 class Categories extends Component
 {
 
-    public $title, $slug, $termId, $updateCat = false, $addCat = false;
+    public $name, $slug, $termId, $updateCat = false, $addCat = false;
 
     public function render()
     {
+        $this->cats = PostCategory::select('term_id', 'name', 'slug')->get();
         return view('livewire.posts.categories');
     }
 
@@ -27,8 +28,8 @@ class Categories extends Component
      * List of add/edit form rules
      */
     protected $rules = [
-        'title' => 'required',
-        'description' => 'required'
+        'name' => 'required',
+        'slug' => 'required'
     ];
  
     /**
@@ -36,19 +37,9 @@ class Categories extends Component
      * @return void
      */
     public function resetFields(){
-        $this->title = '';
-        $this->description = '';
+        $this->name = '';
+        $this->slug = '';
     }
- 
-    /**
-     * render the post data
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    // public function render()
-    // {
-    //     $this->posts = Posts::select('id', 'title', 'description')->get();
-    //     return view('livewire.post');
-    // }
  
     /**
      * Open Add Post form
@@ -57,24 +48,24 @@ class Categories extends Component
     public function addPost()
     {
         $this->resetFields();
-        $this->addPost = true;
-        $this->updatePost = false;
+        $this->addCat = true;
+        $this->updateCat = false;
     }
      /**
       * store the user inputted post data in the posts table
       * @return void
       */
-    public function storePost()
+    public function storeCat()
     {
         $this->validate();
         try {
             Posts::create([
-                'title' => $this->title,
-                'description' => $this->description
+                'name' => $this->name,
+                'slug' => $this->slug
             ]);
-            session()->flash('success','Post Created Successfully!!');
+            session()->flash('success','Category Created Successfully!!');
             $this->resetFields();
-            $this->addPost = false;
+            $this->addCat = false;
         } catch (\Exception $ex) {
             session()->flash('error','Something goes wrong!!');
         }
@@ -85,17 +76,18 @@ class Categories extends Component
      * @param mixed $id
      * @return void
      */
-    public function editPost($id){
+    public function editCat($id) 
+    {
         try {
-            $post = Posts::findOrFail($id);
+            $post = PostCategory::findOrFail($id);
             if( !$post) {
                 session()->flash('error','Post not found');
             } else {
-                $this->title = $post->title;
-                $this->description = $post->description;
-                $this->postId = $post->id;
-                $this->updatePost = true;
-                $this->addPost = false;
+                $this->name = $post->name;
+                $this->slug = $post->slug;
+                $this->termId = $post->term_id;
+                $this->updateCat = true;
+                $this->addCat = false;
             }
         } catch (\Exception $ex) {
             session()->flash('error','Something goes wrong!!');
@@ -107,17 +99,17 @@ class Categories extends Component
      * update the post data
      * @return void
      */
-    public function updatePost()
+    public function updateCat()
     {
         $this->validate();
         try {
-            Posts::whereId($this->postId)->update([
-                'title' => $this->title,
-                'description' => $this->description
+            Posts::whereId($this->termId)->update([
+                'name' => $this->name,
+                'slug' => $this->slug
             ]);
-            session()->flash('success','Post Updated Successfully!!');
+            session()->flash('success','Category Updated Successfully!!');
             $this->resetFields();
-            $this->updatePost = false;
+            $this->updateCat = false;
         } catch (\Exception $ex) {
             session()->flash('success','Something goes wrong!!');
         }
@@ -127,23 +119,23 @@ class Categories extends Component
      * Cancel Add/Edit form and redirect to post listing page
      * @return void
      */
-    public function cancelPost()
+    public function cancelCat()
     {
-        $this->addPost = false;
-        $this->updatePost = false;
+        $this->addCat = false;
+        $this->updateCat = false;
         $this->resetFields();
     }
  
     /**
-     * delete specific post data from the posts table
+     * delete specific post data from the categories table
      * @param mixed $id
      * @return void
      */
-    public function deletePost($id)
+    public function deleteCat($id)
     {
         try{
-            Posts::find($id)->delete();
-            session()->flash('success',"Post Deleted Successfully!!");
+            PostCategory::find($id)->delete();
+            session()->flash('success',"Category Deleted Successfully!!");
         }catch(\Exception $e){
             session()->flash('error',"Something goes wrong!!");
         }
