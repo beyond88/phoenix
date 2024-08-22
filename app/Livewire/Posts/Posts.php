28720b627fa8cb1e8fa7e562e147c7585672964e
beyond88 <muhin.cse.diu@gmail.com>
2024-08-23 01:30:03 +0600

    add new posts livewire

--- app/Livewire/Posts/Posts.php
+++ app/Livewire/Posts/Posts.php
@@ -3,11 +3,15 @@
 namespace App\Livewire\Posts;
 
 use Livewire\Component;
+use App\Models\PostCategory;
 
 class Posts extends Component
 {
+
+    public $cats; 
     public function render()
     {
+        $this->cats = PostCategory::select('term_id', 'name', 'slug')->get();
         return view('livewire.posts.posts');
     }
 }
