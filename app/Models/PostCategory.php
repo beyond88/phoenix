20bb9dd91ccaa9f89d45e071334db7abf1571785
beyond88 <muhin.cse.diu@gmail.com>
2024-08-22 11:00:44 +0600

    add notification for category crud

--- app/Models/PostCategory.php
+++ app/Models/PostCategory.php
@@ -9,27 +9,21 @@ class PostCategory extends Model
 {
     use HasFactory;
 
-    // Specify the table name if it does not follow Laravel's naming convention
     protected $table = 'post_categories';
 
-    // Specify the primary key
     protected $primaryKey = 'term_id';   
 
-    // The primary key is non-incrementing or non-numeric if you have a custom primary key
     public $incrementing = true;
     
-    // The data type of the primary key
     protected $keyType = 'unsignedBigInteger';
 
     protected $casts = [
-        'term_id' => 'integer',  // or simply remove this line if not needed
+        'term_id' => 'integer',
     ];
 
-    // The attributes that are mass assignable
     protected $fillable = [
         'name', 'slug'
     ];
 
-    // Disable timestamps if your table doesn't have 'created_at' and 'updated_at' columns
     public $timestamps = false;
 }
--- resources/views/livewire/posts/categories.blade.php
+++ resources/views/livewire/posts/categories.blade.php
@@ -6,20 +6,36 @@
             <h5 class="text-body-tertiary fw-semibold">Create post categories across your store</h5>
          </div>
          <div class="col-auto">
-            <button class="btn btn-phoenix-secondary me-2 mb-2 mb-sm-0" type="button" wire:click.prevent="cancelCat()">Discard</button>
+            <button class="btn btn-phoenix-secondary me-2 mb-2 mb-sm-0" type="button" wire:click.prevent="cancelCategory()">Discard</button>
             @if ($updateCat)
-                <button class="btn btn-primary mb-2 mb-sm-0" type="button" wire:click.prevent="updateCat()">Update</button>
+                <button class="btn btn-primary mb-2 mb-sm-0" type="button" wire:click.prevent="updateCategory()">Update</button>
             @else
-                <button class="btn btn-primary mb-2 mb-sm-0" type="button" wire:click.prevent="storeCat()">Save</button>
+                <button class="btn btn-primary mb-2 mb-sm-0" type="button" wire:click.prevent="storeCategory()">Save</button>
             @endif
          </div>
       </div>
       <div class="row g-5">
+        <div class="col-12 col-xl-8">
+            @if(session()->has('success'))
+                <div class="alert alert-outline-success d-flex align-items-center" role="alert">
+                    <span class="fas fa-check-circle text-success fs-5 me-3"></span>
+                    <p class="mb-0 flex-1">{{ session()->get('success') }}</p>
+                    <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @if(session()->has('error'))
+                <div class="alert alert-outline-danger d-flex align-items-center" role="alert">
+                    <span class="fas fa-times-circle text-danger fs-5 me-3"></span>
+                    <p class="mb-0 flex-1">{{ session()->get('error') }}</p>
+                    <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+        </div>
          <div class="col-12 col-xl-8">
             <h4 class="mb-3">Name</h4>
             <input class="form-control mb-5 @error('name') is-invalid @enderror" type="text" placeholder="" wire:model="name" />
             <h4 class="mb-3">Slug</h4>
-            <input class="form-control mb-5 @error('slug') is-invalid @enderror" type="text" placeholder="" wire:model="slug" />
+            <input class="form-control mb-5" type="text" placeholder="" wire:model="slug" />
          </div>
          <div class="col-12 col-xl-4">
             <div class="row g-2">
@@ -53,8 +69,8 @@
                                                         0
                                                     </td>
                                                     <td>
-                                                        <button wire:click="editCat({{$cat->term_id}})" class="btn btn-primary me-1 mb-1" type="button">Edit</button>
-                                                        <button wire:click="deleteCat({{$cat->term_id}})"class="btn btn-danger me-1 mb-1" type="button">Delete</button>
+                                                        <button wire:click="editCategory({{$cat->term_id}})" class="btn btn-primary me-1 mb-1" type="button">Edit</button>
+                                                        <button wire:click="deleteCategory({{$cat->term_id}})"class="btn btn-danger me-1 mb-1" type="button">Delete</button>
                                                     </td>
                                                 </tr>
                                             @endforeach
