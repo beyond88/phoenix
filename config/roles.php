2cb5bf73b94ab5a38af4b6d6e56c964d99de3fc5
beyond88 <muhin.cse.diu@gmail.com>
2024-10-10 00:20:49 +0600

    add roles

---
+++ config/roles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'administrator' => 'Administrator',
+    'editor' => 'Editor',
+    'author' => 'Author',
+    'contributor' => 'Contributor',
+    'subscriber' => 'Subscriber',
+];
